0fa60d19c03d96173fb4f836b493a2ef57c29c52
EdamAme-x <edame8080@gmail.com>
2024-11-18 22:12:24 +0900

    update

--- src/components/ui/neon-button/index.tsx
+++ src/components/ui/neon-button/index.tsx
@@ -6,6 +6,7 @@ export const NeonButton = ({ className, children, ...props }: React.ComponentPro
         <button
             className={cn(
                 css.neonButton,
+                "group",
                 className
             )}
             {...props}
@@ -17,9 +18,11 @@ export const NeonButton = ({ className, children, ...props }: React.ComponentPro
                 }}
             ></div>
             {children}
-            <div className="insert-0 absolute inset-x-0 bottom-0 size-full [border-radius:0.7rem] px-4 py-1.5 text-sm font-medium shadow-[inset_0_-8px_10px_#ffffff22] transform-gpu transition-all duration-300 ease-in-out pointer-events-none"></div>
+            <div className="insert-0 absolute inset-x-0 bottom-0 size-full [border-radius:0.7rem] px-4 py-1.5 text-sm font-medium shadow-[inset_0_-8px_10px_#ffffff22] transform-gpu transition-all duration-300 group-hover:shadow-[inset_0_-8px_10px_#ffffff33] ease-in-out pointer-events-none"></div>
+            <span className="absolute -bottom-[1px] left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-violet-400/0 via-violet-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
         </button>
     );
 };
 
 // Created by github.com/EdamAme-x
+

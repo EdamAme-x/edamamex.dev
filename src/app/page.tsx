334db5b4b51aa19f5d6a3ecf136aa2586811d001
EdamAme-x <edame8080@gmail.com>
2024-11-24 14:11:57 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { Sparkles } from "@/components/ui/sparkles";
 import { Hero } from "./_components/hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Sparkles
+        background="transparent"
+        className="fixed top-0 w-screen h-screen"
+        maxSize={1.5}
+        speed={2}
+        particleDensity={5}
+        particleColor={["#7E59FE", "#FF8C00"]}
+      />
     </>
   );
 }
